7747aac102efa837259dd27b4ae00cbbf5b5def3
Tereza Bradkova <tereza.hesikova@seznam.cz>
2023-01-16 14:36:28 +0100

    ukol_1_tereza_bradkova

---
+++ ukol1.py
@@ -0,0 +1,18 @@
+jmeno = "tereza"
+
+print(f"{jmeno}@czechitas.cz")
+
+#nebo
+
+jmeno = input("Zadej sve jmeno")
+print(f"{jmeno}@czechitas.cz")
+
+#nebo
+
+jmeno = "tereza"
+email = jmeno + "@czechitas.cz"
+print(email)
+
+
+
+
