241a5830b7ca05f02f90394375f07684756373fc
Tereza Bradkova <tereza.hesikova@seznam.cz>
2023-02-07 12:44:26 +0100

    ukol4

--- ukol4.py
+++ ukol4.py
@@ -4,10 +4,13 @@ def overeni(cislo):
     if len(cislo) == 9:
         return True
     elif len(cislo) == 13 and cislo[0:4]== "+420":
+        cislo.replace(" ","")
+        return True
+    elif len(cislo) == 16 and cislo[4]== " " and cislo[8]== " " and cislo[12]== " ":
         return True
     else:
         return False
-  #fce cena zpravy  
+#fce cena zpravy  
 def cena_zpravy(text):
     cena = 3
     pocet_znaku = int(len(text))
@@ -25,6 +28,8 @@ else:
     zprava = input("Zadej text zprávy.")
     vysledna_cena = cena_zpravy(zprava)
     print(f"Cena zprávy bude {vysledna_cena} Kč.")
+
+
 
 
 
