67efe0f336b044bf977843e99c9543467c4e1fd7
Tereza Bradkova <tereza.hesikova@seznam.cz>
2023-02-22 13:00:02 +0100

    ukol 3 oprava

--- ukol3.py
+++ ukol3.py
@@ -5,24 +5,20 @@ with open('body.json', encoding='utf-8') as muj_soubor:
     body = json.load(muj_soubor)
 
 print(body)
+prospech = {}
 
 for key, value in body.items():
     if value >= 60:
         vysledek = "pass"
+        prospech[key] = vysledek
     else:
         vysledek = "fail"
-
-
-prospech = {}
-
-for key in body:
-
-    prospech[key] = vysledek
+        prospech[key] = vysledek
 
 print(prospech)
 
 with open('prospech.json', mode = 'w', encoding='utf-8') as souborr:
-    json.dump(prospech,souborr)
+    json.dump(prospech,souborr, ensure_ascii=False)
 
     
 
